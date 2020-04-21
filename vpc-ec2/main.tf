5c1fc615dcf09ce4ea7242d38a7737510f9ef82f
0ky3r43m78m070b@gmail.com <0ky3r43m78m090b@gmail.com>
2020-04-21 11:33:23 +0900

    Add terraform.state in S3

--- vpc-ec2/main.tf
+++ vpc-ec2/main.tf
@@ -1,4 +1,15 @@
 provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
+
+terraform {
+  required_version = ">= 0.12.0"
+  backend "s3" {
     profile = "terraform"
-    region = "ap-northeast-1"
+    region  = "ap-northeast-1"
+    bucket  = "terraform-tfstate-runble1"
+    key     = "vpc-ec2/terraform.tfstate"
+    encrypt = true
+  }
 }
